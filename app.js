71a21c3709735dda38a073b039883c7c27b7c61a
earanaw <earanaw@gmail.com>
2024-07-25 22:52:51 -0500

    Segudo commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import {Telegraf} from'telegraf';
 const bot = new Telegraf('5081397447:AAFIdvUzyXDNy-UA8zg3yMtBOVmVY_bvOsQ');
 
 bot.start((ctx) => ctx.reply('Bienvenido!'));
-bot.help((ctx) => ctx.reply('Cómo puedo ayudarte?'));
+bot.help((ctx) => ctx.reply('Cómo puedo ayudarte amigo?'));
 bot.on('text', (ctx) => {
     ctx.reply(`Recibido tu mensaje: ${ctx.message.text}`);
 });
