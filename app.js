9280ca97fb078c320977b7e60544f0cf80b7e09b
earanaw <earanaw@gmail.com>
2024-07-25 22:27:18 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import {Telegraf} from'telegraf';
+
+const bot = new Telegraf('5081397447:AAFIdvUzyXDNy-UA8zg3yMtBOVmVY_bvOsQ');
+
+bot.start((ctx) => ctx.reply('Bienvenido!'));
+bot.help((ctx) => ctx.reply('Cómo puedo ayudarte?'));
+bot.on('text', (ctx) => {
+    ctx.reply(`Recibido tu mensaje: ${ctx.message.text}`);
+});
+
+bot.launch();
+console.log('Bot de Telegram iniciado');
